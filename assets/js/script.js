e6afe196ea3cf6594cb0818bff358e7b940d47b4
aungkaungmyatpaing <saberaozo5152@gmail.com>
2020-08-10 09:38:56 +0630

    upload project files.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var menu_btn=document.querySelector(".menu_icon");
+menu_btn.addEventListener("click",function(){
+    alert("Hello");
+});
+
---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple_blog</title>
+    <script src="https://kit.fontawesome.com/eda44fcafb.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/theme.css">
+</head>
+<body>
+<!-- navbar ========================================================= Start -->
+    <div class="navbar">
+        <div class="logo_container">
+            <img src="img/logo.png" class="navbar_logo">
+        </div>
+        <div class="menu_icon">
+                <i class="fas fa-align-center"></i>
+        </div>
+
+        <form action=""  class="search_container">
+            <input type="text" placeholder="SEARCH.." name="search" class="search_box">
+                <button type="submit" class="search_button"><i class="fas fa-search"></i></button>
+        </form>
+        <div class="menu_links">
+            <div class="home_page">Home</div>
+            <div class="contact_page">Contact Us</div>
+            <div class="log_out">Log Out</div>
+        </div>
+    </div>
+    <!-- navbar_menu_dropdown ============================================================================= -->
+        <div class="drop_down">
+            <form action="" class="search_container_drop_down">
+                <input type="text" placeholder="SEARCH..." name="search" class="search_box_drop_down">
+                <button type="submit" class="search_button_drop_down"><i class="fas fa-search"></i></button>
+            </form>
+            <div class="sign">
+
+                <div class="sign_icon_container">
+                    <i class="fas fa-sign-in-alt"></i>
+                    <span>Sign In</span>
+                </div>
+
+                <div class="sign_icon_container">
+                    <i class="fas fa-download"></i>
+                    <span>Download</span>
+                </div>
+
+                <div class="sign_icon_container">
+                    <i class="fas fa-upload"></i>
+                    <span>Upload</span>
+                </div>
+
+                <div class="sign_icon_container">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span>Sign Out</span>
+                </div>
+                
+            </div>
+
+            <div class="customer_contact">
+                <div class="get_premium">
+                    <span>
+                    <i class="far fa-star"></i>
+                    </span>
+                    <span>Get Premium</span>
+                </div>
+                <div class="live_stream">
+                    <span>
+                    <i class="fas fa-video"></i>
+                    </span>
+                    <span>Live Stream</span>
+                </div>
+                <div class="save_list">
+                    <span>
+                    <i class="far fa-bookmark"></i>
+                </span>
+                    <span>Save Lists</span>
+                </div>
+                <div class="contact">
+                    <span>
+                        <i class="fab fa-teamspeak"></i>
+                    </span>
+                    <span>Contact to Team</span>
+                </div>
+            </div>
+        </div>
+
+<!-- navbar========================================================== End -->
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<h1>PornHub</h1>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
